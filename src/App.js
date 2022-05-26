55a3e4269f0add0817f2836dabb9c863d716af08
Willyam Ramos <ramoswillyam1@gmail.com>
2022-05-25 20:22:11 -0700

    Added Components To App

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GetType from "./components/GetType";
 import Main from "./components/Main";
 import GetService from "./components/GetService";
-import GetResults from "./components/GetResults";
+import SearchBar from "./components/SearchBar";
+import ResultsList from "./components/ResultsList";
+import Navigation from "./components/Navigation";
 import './App.css';
 
 
@@ -16,13 +18,15 @@ const App = () => {
                         <Route path='/' element={<Main />} />
                         <Route path='/type' element={<GetType />} />
                         <Route path='/service' element={<GetService />} />
-                        <Route path='/results' element={<GetResults />} />
+                        <Route path='/search' element={<SearchBar />} />
+                        <Route path='/results' element={<ResultsList />} />
                     </Routes>
                 </div>
+                <Navigation />
             </BrowserRouter>
         </div>
     )
-}
+};
 
 
 export default App;
