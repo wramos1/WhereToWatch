04a41c7b87347016bb84e1f3d17da103bdc97a2c
Willyam Ramos <ramoswillyam1@gmail.com>
2022-05-25 19:25:01 -0700

    Integrated Navigation Buttons

---
+++ src/components/Navigation.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Navigation = () => {
+    const navigate = useNavigate();
+
+    const urlElements = window.location.href.split('/');
+
+    const goBack = () => {
+        if (urlElements[3] !== '') {
+            return (
+                <button
+                    style={{
+                        position: 'fixed',
+                        bottom: '0',
+                        left: '0',
+                        fontSize: '1.4em',
+                        cursor: 'pointer',
+                        color: 'rgb(31, 199, 177)',
+                        backgroundColor: 'black'
+                    }}
+                    onClick={() => navigate(-1)}
+                >
+                    Back
+                </button>
+            );
+        };
+    };
+
+    const startOver = () => {
+        if (urlElements[3] === 'results') {
+            return (
+                <button
+                    style={{
+                        position: 'fixed',
+                        bottom: '0',
+                        right: '0',
+                        fontSize: '1.4em',
+                        cursor: 'pointer',
+                        color: ' rgb(31, 199, 177)',
+                        backgroundColor: 'black'
+                    }}
+                    onClick={() => window.location.replace('/')}
+                >
+                    Start Over
+                </button>
+            );
+        };
+    };
+
+    return (
+        <div>
+            {goBack()}
+            {startOver()}
+        </div>
+    );
+};
+
+export default Navigation;
